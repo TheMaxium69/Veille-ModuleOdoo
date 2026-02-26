177286870c83c61e6c70bdccc8e38a9435cad60b
TheMaxium69 <mxm.tournier@gmail.com>
2026-02-27 00:56:00 +0100

    update

--- models/agence_vehicule.py
+++ models/agence_vehicule.py
@@ -1,12 +1,21 @@
 from odoo import models, fields
 
+
+class AgenceVehiculeStatus(models.Model):
+    _name = 'agence.vehicule.status'
+    _description = 'Statut du Véhicule'
+
+    name = fields.Char('Nom du Statut', required=True)
+    color = fields.Integer('Couleur')
+
+
 class AgenceVehicule(models.Model):
     _name = 'agence.vehicule'
-    # Les caractéristiques des véhicules
+
     marque = fields.Char('Marque', required=True)
     modele = fields.Char('Modèle', required=True)
     date_achat = fields.Date('Date d Achat')
-    chaffeur_ids = fields.Many2many(
-        'res.partner',
-        string='Chaffeurs'
-    )
+
+    status_id = fields.Many2one('agence.vehicule.status', string='Statut')
+
+    chaffeur_ids = fields.Many2many('res.partner', string='Chaffeurs')
